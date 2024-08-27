45c81ec1fbe9858dc8690dcce13bf63197ac4a35
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-08-27 10:50:58 -0400

    Update Pitcher Stats.py

--- Pitchers/Pitcher Stats.py
+++ Pitchers/Pitcher Stats.py
@@ -1,9 +1,19 @@
 import pandas as pd
+from scipy import stats
 
+#Original File Name
 old_path = '3-3File.csv'
 data = pd.read_csv(old_path)
 
-pitcher_data = data[data['Pitcher']] == "Kuhle, Aiden"
+#Pitcher Name
+pitcher_name = 'Colosimo, Dominic'
+
+
+filtered_data = data[data['Pitcher'] == pitcher_name]
+
+new_data = filtered_data.describe()
+
+print(new_data)
 
 
 
