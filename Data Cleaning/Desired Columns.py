b0c789d5c6d92ec6e69280078363247472067f3b
lkarpeo2323 <123440718+lkarpeo2323@users.noreply.github.com>
2024-08-27 10:34:41 -0400

    Create Desired Columns.py

---
+++ Data Cleaning/Desired Columns.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+#The file you are trying to clean
+old_path = '3-3File.csv'
+
+data = pd.read_csv(old_path)
+
+#List the columns you wanna keep
+keep = [
+'Pitcher',
+'Batter',
+'Balls',
+'Strikes',
+'RelSpeed',
+'Tilt'
+'ExitSpeed',
+'Angle'
+    ]
+
+kept = data[keep]
+
+#desired new file path
+new_path = 'CleanedBaseball.csv'
+
+kept.to_csv(path,index=False)
+
+print(f"The data has been cleaned to {new_path}")
+        
+        
+            
+                  
+            
+              
